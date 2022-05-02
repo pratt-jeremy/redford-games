da6e493b613f174db174c476b835d46470cbd7c6
pratt-jeremy <pratt.jeremy1@gmail.com>
2022-05-02 17:40:44 -0500

    added error handling and guess couter

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
 from random import randint
+from art import *
 
+guesses = 10
 
-guess = input('Guess a number between 1 and 1000: ')
-answer = randint(1,1000)
+print("Guesses Left: " + str(guesses))
+guess = int(input('Guess a number between 1 and 100: '))
+answer = randint(1,100)
 while guess != answer:
-    guess = input('Try Again: ')
+    if guesses == 0:
+        print("YOU FAILED!!")
+        print("The Answer Was: " + str(answer))
+        print(art("angry"))
+    else:
+        guesses = guesses - 1
+        if guess > answer:
+            print("Guesses Left: " + str(guesses))
+            print(art("cat"))
+            guess = int(input('Too High: '))
+        if guess < answer:
+            print("Guesses Left: " + str(guesses))
+            print(art("sad"))
+            guess = int(input('Too Low: '))
 
-print('Correct! ' + guess + ' is the answer!')
+print(art("happy"))
+print('Correct! ' + str(guess) + ' is the answer!')
